f252d7c596c53d98b960db63641236c03c5ccc4e
Sarangnadh <102402726+Sarangnadh@users.noreply.github.com>
2025-06-08 00:30:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const connectDB = require('./db');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
